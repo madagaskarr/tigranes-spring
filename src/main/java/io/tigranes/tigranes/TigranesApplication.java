66f83e828b94a00511e9946bb50a11a47fc79390
tigran <me@tigranes.io>
2020-07-04 23:06:48 -0700

    Initial commit

---
+++ src/main/java/io/tigranes/tigranes/TigranesApplication.java
@@ -0,0 +1,13 @@
+package io.tigranes.tigranes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigranesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TigranesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/tigranes/tigranes/TigranesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tigranes.tigranes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TigranesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
